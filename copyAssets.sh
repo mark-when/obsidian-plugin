6d1ff7032b11f53a4d3501abbcf481d2ef2ad61e
Rob Koch <kochrt@gmail.com>
2023-09-14 15:27:31 -0400

    copy styles over

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ./main.js ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen
-cp ./assets/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen/assets
+cp ./main.js ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen
+cp ./assets/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen/assets
+cp ./styles.css ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen
