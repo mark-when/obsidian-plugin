f6f5dfe409cd36e560db7f2df3c611e62ba25bd6
Rob Koch <kochrt@gmail.com>
2024-04-18 02:55:33 -0700

    copy everything from out

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./main.js ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen
+cp ./out/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen
 cp ./assets/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen/assets
 cp ./node_modules/@markwhen/timeline/dist/index.html ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen/assets/timeline.html
 cp ./styles.css ~/Documents/Obsidian\ Vault/.obsidian/plugins/obsidian-markwhen
