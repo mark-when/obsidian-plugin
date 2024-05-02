f823bdbee95f46708c95363e2cbf37c0fbe4087c
Rob Koch <kochrt@gmail.com>
2024-05-01 18:06:05 -0700

    Remove asset copy line

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cp ./out/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen
-cp ./assets/* ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen/assets
 cp ./styles.css ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen
 cp ./manifest.json ~/Documents/Obsidian\ Vault/.obsidian/plugins/markwhen
